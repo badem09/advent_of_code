cb1007484cec97f0c62b6527182b66fe3fed5f48
Demba BA <badembajob@gmail.com>
2023-12-02 08:09:59 +0100

    cleaning and link

--- 2023/day2/day2.py
+++ 2023/day2/day2.py
@@ -1,15 +1,15 @@
+#https://adventofcode.com/2023/day/2
 from functools import reduce
 
 input = open("input.txt", "r")
-r, g, b, = 12, 13, 14
-constraint = {'red' : 12, 'green' : 13, 'blue' : 14}
-max_col = {'red' : 0, 'green' : 0, 'blue' : 0}
+constraint = {'red': 12, 'green': 13, 'blue': 14}
+min_col = {'red': 0, 'green': 0, 'blue': 0}
 sum_part1 = 0
 sum_part2 = 0
 
 for l in input:
-    l = l.replace('\n','')
-    id = l.split(":")[0].split(' ')[1]
+    l = l.replace('\n', '')
+    game_id = l.split(":")[0].split(' ')[1]
     games = [e.split(',') for e in l.split(':')[1].split(";")]
     valid_game = True
     for game in games:
@@ -17,14 +17,14 @@ for l in input:
             nb = int(cube.split(" ")[1])
             col = cube.split(" ")[2]
             valid_game = False if nb > constraint[col] else valid_game
-            max_col[col] = nb if nb > max_col[col] else max_col[col]
+            min_col[col] = nb if nb > min_col[col] else min_col[col]
 
     if valid_game:
-        sum_part1 += int(id)
+        sum_part1 += int(game_id)
 
-    min_nb = [e[1] for e in max_col.items()]
+    min_nb = [e[1] for e in min_col.items()]
     sum_part2 += reduce(lambda x,y: x*y, min_nb) # π (prod)
-    for col in max_col.keys():
-        max_col[col] = 0
+    for col in min_col.keys():
+        min_col[col] = 0
 
 print(sum_part1, sum_part2)
