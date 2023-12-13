be2a6c1e58bce3a020fdf325898b061753d8d785
Demba BA <badembajob@gmail.com>
2023-12-13 17:24:57 +0100

    Part 1 ok; Part 2 debug

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -1,42 +1,71 @@
 import copy
-from pprint import pprint
 
-from IPython.display import display
 input = open('input.txt','r').read()
 
-input_l = [e.split('\n') for e in input.split('\n\n')]
-input_l = [[list(e) for e in row] for row in input_l ]
+input_l = [[list(e) for e in row] for row in [e.split('\n') for e in input.split('\n\n')] ]
 
 left_v = []
 above_h = []
+left_v2 = []
+above_h2 = []
 
+def nb_errors(liste1, liste2):
+    r = 0
+    for i in range(min(len(liste1),len(liste2))):
+        if liste1[i] != liste2[i]:
+            r+=1
+    return r
+
+def fix_smudge (pattern):
+    # find l'axe qui a quune erreur
+    for i in range(len(pattern) - 1):  # vertical line
+        if pattern[i] == pattern[i + 1]:
+            left = pattern[0:i][::-1]
+            right = pattern[i + 2:]
+            nb_err = nb_errors(left, right)
+            if nb_err == 1:
+                return len(left) + 1
+    return -1
 
 def find_line(pattern):
     for i in range(len(pattern) - 1):  # vertical line
         if pattern[i] == pattern[i + 1]:
-            left = pattern[0:i]
+            left = pattern[0:i][::-1]
             right = pattern[i + 2:]
-            k = 0
-            for j in range(min(len(left), len(right))):
-                if left[len(left) - j - 1] != right[j]:
-                    return []
-                k += 1
-            return i + 1
-    return []
+            nb_err = nb_errors(left,right)
+            if nb_err == 0 or i in (0,len(pattern)-1):
+                return len(left) + 1
+    return -1
 
 
 for pattern in input_l:
     res = find_line(pattern)
-    if res == []:
-        pattern = list(map(list, zip(*copy.deepcopy(pattern))))
-        #3e pattern trouve pas de left v
-        res = find_line(pattern)
+    if res == -1: # if no horizontal symetry
+        pattern1 = list(map(list, zip(*copy.deepcopy(pattern))))
+        res = find_line(pattern1)
         left_v.append(res)
     else :
         above_h.append(res)
 
-print(sum([e for e in left_v if isinstance(e,int)]+[e*100 for e in above_h if isinstance(e,int)]))
-print(above_h, '\n',left_v)
+    res2 = fix_smudge(pattern)
+    if res2 == -1:
+        pattern2 = list(map(list, zip(*copy.deepcopy(pattern))))
+        res2 = fix_smudge(pattern2)
+        left_v2.append(res2)
+    else:
+        above_h2.append(res2)
+
+
+
+
+
+
+
+print("partie 1 :",sum([e for e in left_v if isinstance(e,int)]+[e*100 for e in above_h if isinstance(e,int)]))
+print("partie 2 :",sum([e for e in left_v2 if isinstance(e,int)]+[e*100 for e in above_h2 if isinstance(e,int)]))
+print(left_v2)
+print(above_h2)
+
 
 
 
